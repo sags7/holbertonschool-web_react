76065f21f844ca604997241a13ea3566984b8fed
sag <juansebastianaramburo@gmail.com>
2025-05-29 11:45:07 -0500

    add printTeacher() and interface printTeacherFunction

--- TypeScript/task_1/js/main.ts
+++ TypeScript/task_1/js/main.ts
@@ -12,3 +12,11 @@ interface Teacher {
 interface Directors extends Teacher {
   numberOfReports: number;
 }
+
+function printTeacher(firstName: string, lastName: string) {
+  return `${firstName[0]}. ${lastName}`;
+}
+
+interface printTeacherFunction {
+  printTeacher(firstName: string, lastName: string): string;
+}
